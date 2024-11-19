ae8345f37d863f6d4c7e597f47f7c6e110746a65
Anam Arif <anamarif443@gmail.com>
2024-11-20 02:37:13 +0500

    Updated files with line-ending adjustments

--- routes/firmeneintrag.py
+++ routes/firmeneintrag.py
@@ -1,11 +1,12 @@
 from fastapi import Depends, APIRouter, HTTPException
-from typing import List, Optional
-import pandas as pd
-router = APIRouter()
+from typing import List, Optional ## to be used for type hinting
+import pandas as pd ## to manipulate the csv
+
+
+router = APIRouter() ## instance of APIRouter so we can define routes ahead
 
 # Load the CSV file into a DataFrame
 df = pd.read_csv('scraped_info.csv')
-
 
 @router.get("/search/", response_model=List[dict])
 async def search_csv(
@@ -26,3 +27,30 @@ async def search_csv(
         raise HTTPException(status_code=404, detail="No results found")
 
     return results
+
+@router.get("/get_cik/", response_model=dict)
+async def get_cik(name: str):
+
+    filtered_df = df[df['name'].str.contains(name, case=False, na=False, regex=False)]
+    
+    if filtered_df.empty:
+        raise HTTPException(status_code=404, detail="No company found with that name")
+    
+    cik_code = filtered_df.iloc[0]['cik']
+
+    return {"name": name, "cik": cik_code}
+
+
+@router.get("/get_company_info/", response_model=dict) ### get method, return in dictionary form
+async def get_company_info(cik: str): ## asynchronous with input in str format
+
+    # Filter the DataFrame by the provided CIK
+    filtered_df = df[df['cik'] == cik]
+    
+    if filtered_df.empty:
+        raise HTTPException(status_code=404, detail="No company found with that CIK code")
+    
+    # Convert the information to a dictionary
+    company_info = filtered_df.iloc[0].to_dict() ## return all company information
+    
+    return company_info
